9e95de3fdc742e157d8b4295491f988c9e13dbb3
Milosz11 <mildab141@gmail.com>
2024-06-16 11:33:12 -0500

    Finish complete screen

--- app/src/main/java/com/example/taskmanagercompletescreen/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagercompletescreen/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanagercompletescreen.ui.theme.TaskManagerCompleteScreenTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskManagerCompleteScreenTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    CompleteScreenApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CompleteScreenApp(modifier: Modifier = Modifier) {
+    CompleteScreen(
+        image = painterResource(id = R.drawable.ic_task_completed),
+        successMessage = stringResource(R.string.text_success),
+        complementMessage = stringResource(R.string.text_complement),
         modifier = modifier
+            .fillMaxSize()
     )
+}
+
+@Composable
+fun CompleteScreen(
+    image: Painter,
+    successMessage: String,
+    complementMessage: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = successMessage,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = complementMessage,
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CompleteScreenPreview() {
     TaskManagerCompleteScreenTheme {
-        Greeting("Android")
+        CompleteScreenApp()
     }
 }
